70870a4a813997087426716314218da98b9c1c34
ovynash504-max <barikabinash504@gmail.com>
2026-04-23 14:51:40 +0530

    Create news.js

---
+++ news.js
@@ -0,0 +1,9 @@
+const newsData = [
+{
+id: 1,
+title: "India Update 🚨",
+short: "Naya rule aaya",
+full: "Aaj government ne ek important decision liya hai jo logon ko affect karega.",
+img: "https://images.unsplash.com/photo-1504711434969-e33886168f5c"
+}
+];
